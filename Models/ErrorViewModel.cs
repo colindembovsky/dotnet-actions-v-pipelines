bfb9b8a4db2c63db901f3cf4121fe34967a07a58
Colin Dembovsky <colindembovsky@gmail.com>
2021-10-19 14:34:48 +0000

    Initial code commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace dotnet_actions_v_pipelines.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
