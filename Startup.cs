0331d51cc39326ca2aaafdbfee967856161c17ee
Colin Dembovsky <colindembovsky@gmail.com>
2021-10-19 14:44:03 +0000

    fix startup for codespaces

--- Startup.cs
+++ Startup.cs
@@ -38,8 +38,9 @@ namespace dotnet_actions_v_pipelines
                 app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
+                app.UseHttpsRedirection();
             }
-            app.UseHttpsRedirection();
+            
             app.UseStaticFiles();
 
             app.UseRouting();
