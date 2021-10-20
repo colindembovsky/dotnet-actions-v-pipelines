9f5dace83f2f5573a838af4b2fada30a0ca14292
Colin Dembovsky <colindembovsky@gmail.com>
2021-10-20 10:47:54 -0500

    Update aci.sh

--- infra/aci.sh
+++ infra/aci.sh
@@ -9,4 +9,4 @@ az group create -n $rg -l southcentralus
 az container create -g $rg --name $aci_name --image $image --dns-name-label $aci_name
 
 fqdn=$(az container show -n $aci_name -g $rg --query "ipAddress.fqdn" -o tsv)
-echo "::set-output name=fqdn::$fqdn"
+echo "::set-output name=fqdn::http://$fqdn"
