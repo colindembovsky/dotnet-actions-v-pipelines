49f54a340af1138c6ccdf29a9184a785fd935778
Colin Dembovsky <colindembovsky@gmail.com>
2021-10-21 15:10:21 +0000

    Adding secret

--- infra/aci.sh
+++ infra/aci.sh
@@ -4,9 +4,15 @@ set -euxo pipefail
 rg=$1
 aci_name=$2
 image=$3
+secret=$4
 
 az group create -n $rg -l southcentralus
-az container create -g $rg --name $aci_name --image $image --dns-name-label $aci_name
+az container create -g $rg --name $aci_name --image $image --dns-name-label $aci_name --environment-variables secrets__environment=$secret
 
 fqdn=$(az container show -n $aci_name -g $rg --query "ipAddress.fqdn" -o tsv)
+
+# output for Actions
 echo "::set-output name=fqdn::http://$fqdn"
+
+# output for Pipelines
+echo "##vso[task.setvariable variable=fqdn;]$fqdn"
--- src/Controllers/HomeController.cs
+++ src/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
 using dotnet_actions_v_pipelines.Models;
 
 namespace dotnet_actions_v_pipelines.Controllers
@@ -12,14 +13,17 @@ namespace dotnet_actions_v_pipelines.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IConfiguration _configuration;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
         {
             _logger = logger;
+            _configuration = configuration;
         }
 
         public IActionResult Index()
         {
+            ViewData["Secret"] = _configuration["secrets:environment"];
             return View();
         }
 
